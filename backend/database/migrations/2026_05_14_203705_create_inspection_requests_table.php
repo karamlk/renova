8f262984836bd9ee79c83fb84400c68527bb5968
ebaaahaj004 <beboabosh4gmail.com>
2026-06-22 13:41:44 +0300

    site visits

--- backend/database/migrations/2026_05_14_203705_create_inspection_requests_table.php
+++ backend/database/migrations/2026_05_14_203705_create_inspection_requests_table.php
@@ -1,1 +1,38 @@
 
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('inspection_requests', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('reconstruction_request_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('contractor_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            $table->enum('status', [
+                'pending',
+                'accepted',
+                'rejected',
+            ])->default('pending');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('inspection_requests');
+    }
+};
