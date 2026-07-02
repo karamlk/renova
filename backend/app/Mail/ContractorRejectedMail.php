0ed83019910db32159a52a7e5e95e89f5b8c0d0a
ebaaahaj004 <beboabosh4gmail.com>
2026-07-02 16:40:18 +0300

    roles

--- backend/app/Mail/ContractorRejectedMail.php
+++ backend/app/Mail/ContractorRejectedMail.php
@@ -4,10 +4,7 @@ namespace App\Mail;
 
 use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class ContractorRejectedMail extends Mailable
@@ -16,45 +13,15 @@ class ContractorRejectedMail extends Mailable
 
     public $user;
 
-    public function __construct(
-        User $user
-    ) {
+    public function __construct(User $user)
+    {
         $this->user = $user;
     }
 
     public function build()
     {
-        return $this->subject(
-            'تم رفض تسجليك في المنصة راجع معلوماتك الشخصية و حاول مرة اخرى '
-        )
-            ->view(
-                'emails.contractor-approved'
-            );
-    }
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Contractor Rejected Mail',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this
+            ->subject('تم رفض تسجيلك في المنصة')
+            ->view('emails.contractor-rejected');
     }
 }
