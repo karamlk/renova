5fa1baa686a3023d095d76e10acc8405ac51324f
ebaaahaj004 <beboabosh4gmail.com>
2026-08-18 05:44:07 +0300

    project

--- backend/app/Http/Controllers/ProjectController.php
+++ backend/app/Http/Controllers/ProjectController.php
@@ -111,9 +111,9 @@ class ProjectController extends Controller
             'form.reconstructionRequest',
             'tasks',
             'review',
-            'user',
-            'engineer',
-            'contractor',
+            'user.profile',
+            'engineer.engineerProfile',
+            'contractor.contractorProfile',
         ])->findOrFail($id);
 
         return response()->json([
